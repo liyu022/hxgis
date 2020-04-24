21f6af8e20eb73341c7c3fba1fbf7b28ce482607
thinkpad <thinkpad>
2020-04-24 15:33:03 +0800

    bodytyle

--- src/main.js
+++ src/main.js
@@ -5,14 +5,6 @@ import store from './store'
 import elementUI from 'element-ui'
 Vue.use(elementUI)
 Vue.config.productionTip = false
-/*
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
-*/
-/*  */
 new Vue({
   el: '#app',
   router: router,
